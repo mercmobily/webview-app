a1e9c13a4e7bba46a34b44fdb447b386b2b39c6e
Tony Mobily <tonymobily@gmail.com>
2025-10-19 22:58:05 +0800

    More fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.mathacademy.secure"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mathacademy.secure"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
